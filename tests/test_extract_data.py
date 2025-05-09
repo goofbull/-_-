3a7459c10e91f1b1a428b78030a11db60855ecb8
Kornil17 <dimitra4@mail.ru>
2025-05-09 20:17:33 +0300

    fix tests

--- tests/test_extract_data.py
+++ tests/test_extract_data.py
@@ -1,38 +1,35 @@
+import re
 import unittest
 from unittest.mock import patch
 from extract_data import lemmatize_text, extract_articles, ustanovil, get_data_from_file, get_dict_with_data
 
 class TestCourtCaseProcessing(unittest.TestCase):
 
-    @patch('extract_data.CleanText')
-    def test_get_data_from_file(self, mock_clean_text):
-        # Мокаем возвращаемые значения
-        mock_clean_text.return_value = ("cleaned_text", "judge_name")
-
-        result = get_data_from_file("some_directory", 1)
-
-        self.assertEqual(result, ("cleaned_text", "judge_name"))
-
     @patch('extract_data.get_data_from_file')
     def test_get_dict_with_data(self, mock_get_data):
+        number_of_docs = "1"
         # Мокаем возвращаемые данные
-        mock_get_data.return_value = ("cleaned_text", "judge_name")
+        mock_get_data.return_value = {"cleaned_text": "judge_name"}
 
-        result = get_dict_with_data("some_directory")
+        result = get_dict_with_data("some_directory", 1)
 
-        self.assertEqual(result, {'cleaned_text': 'judge_name'})
+        self.assertEqual(result, {number_of_docs: {"cleaned_text": "judge_name"}})
 
     def test_extract_articles(self):
-        result = extract_articles("some_text_with_article_codes")
-        self.assertEqual(result, {'зк': [12, 13]})
+        article_pattern = re.compile(r"(\d+)\s+(\d+)", re.IGNORECASE)
+        text = "12 13"
+        result = extract_articles(text, article_pattern)
+        self.assertEqual(result, {'13': [12]})
 
     def test_lemmatize_text(self):
         result = lemmatize_text("судья рассмотреть дело")
         self.assertEqual(result.capitalize(), "Судья рассмотреть дело")
 
     def test_ustanovil(self):
-        result = ustanovil("Компания подала иск")
-        self.assertEqual(result[0], "Компания")
+        text = "ООО Компания подала иск в суд"
+        result = ustanovil(" ", text, " ")
+        self.assertEqual("ООО", result[0])
+
 
 if __name__ == '__main__':
     unittest.main()
